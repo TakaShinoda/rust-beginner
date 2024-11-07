04d51fbef5218834dfa61bb1b4901b16f6b3114c
Takahiro <tkhr_621@docomo.ne.jp>
2024-11-08 00:34:43 +0900

    マルチスレッド

--- src/main.rs
+++ src/main.rs
@@ -239,6 +239,37 @@ fn main() {
     for (n, m) in v.iter().zip(s.iter()) {
         println!("({n}, {m})");
     }
+
+    // スレッドの生成
+    let handler = std::thread::spawn(worker);
+
+    // join でスレッドの終了を待ち合わせ
+    match handler.join() {
+        Ok(n) => println!("{n}"),
+        Err(e) => println!("{:?}", e),
+    }
+
+    // チャネルを生成
+    let (tx, rx) = std::sync::mpsc::sync_channel(64);
+
+    // スレッドを生成し rx から受信
+    let handler = std::thread::spawn(move || match rx.recv() {
+        Ok((x, y)) => println!("{}, {}", x, y),
+        Err(e) => eprintln!("{e}"),
+    });
+
+    // チャネルに送信
+    if let Err(e) = tx.send((10, 20)) {
+        eprint!("{e}");
+    }
+
+    // 待ち合わせ
+    if let Err(e) = handler.join() {
+        eprint!("{:?}", e);
+    }
+
+    single_threaded();
+    multi_threaded();
 }
 
 // fn a() -> bool {
@@ -369,4 +400,93 @@ impl PCSpec {
             storage,
         }
     }
+}
+
+fn worker() -> u32 {
+    println!("worker!");
+    100
+}
+
+struct XOR64 {
+    x: u64,
+}
+
+impl XOR64 {
+    fn new(seed: u64) -> XOR64 {
+        XOR64 {
+            x: seed ^ 88172645463325252,
+        }
+    }
+
+    // 擬似乱数生成
+    fn next(&mut self) -> u64 {
+        let x: u64 = self.x;
+        let x = x ^ (x << 13);
+        let x = x ^ (x >> 7);
+        let x = x ^ (x << 17);
+        self.x = x;
+        x
+    }
+}
+
+const NUM: usize = 200000000;
+
+fn randomized_vec() -> (Vec<u64>, Vec<u64>) {
+    let mut v1 = Vec::new();
+    let mut v2 = Vec::new();
+
+    let mut generator = XOR64::new(1234);
+
+    // 乱数生成
+    for _ in 0..NUM {
+        let x1 = generator.next();
+        let x2 = generator.next();
+        v1.push(x1);
+        v2.push(x2);
+    }
+    (v1, v2)
+}
+
+fn single_threaded() {
+    let (mut v1, mut v2) = randomized_vec();
+
+    let start = std::time::Instant::now(); // 現在時刻
+
+    v1.sort();
+    v2.sort();
+
+    let end = start.elapsed(); // 経過時間
+
+    println!(
+        "single_threaded: {}.{:03}秒",
+        end.as_secs(),
+        end.subsec_nanos()
+    )
+}
+
+fn multi_threaded() {
+    let (mut v1, mut v2) = randomized_vec();
+
+    let start = std::time::Instant::now(); // 現在時刻
+
+    let handler1 = std::thread::spawn(move || {
+        v1.sort();
+        v1
+    });
+
+    let handler2 = std::thread::spawn(move || {
+        v2.sort();
+        v2
+    });
+
+    let _v1 = handler1.join().unwrap();
+    let _v2 = handler2.join().unwrap();
+
+    let end = start.elapsed();
+
+    println!(
+        "multi_threaded: {}.{:03}秒",
+        end.as_secs(),
+        end.subsec_nanos()
+    )
 }
